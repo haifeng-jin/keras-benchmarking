9d83f8de991cd6db42fe34df075e2a7906a30951
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-12-13 00:20:13 +0000

    update

--- src/bert_keras.py
+++ src/bert_keras.py
@@ -5,6 +5,35 @@ import tensorflow_datasets as tfds
 import benchmark
 
 
+# Make sure our optimization strategy is the same.
+# In particular AdamW with exluded terms from the bias decay.
+class BertOptimizer(keras.optimizers.AdamW):
+    def __init__(
+        self,
+        learning_rate=5e-5,
+        weight_decay=0.01,
+        beta_1=0.9,
+        beta_2=0.999,
+        epsilon=1e-6,
+        global_clipnorm=1.0,
+        exclude_bias_from_decay=True,
+        exclude_layernorm_from_decay=True,
+    ):
+        super().__init__(
+            learning_rate=learning_rate,
+            weight_decay=weight_decay,
+            beta_1=beta_1,
+            beta_2=beta_2,
+            epsilon=epsilon,
+            global_clipnorm=global_clipnorm,
+        )
+        if exclude_bias_from_decay:
+            self.exclude_from_weight_decay(var_names=["bias"])
+        if exclude_layernorm_from_decay:
+            self.exclude_from_weight_decay(var_names=["gamma"])
+            self.exclude_from_weight_decay(var_names=["beta"])
+
+
 def get_dataset():
     preprocessor = keras_nlp.models.BertPreprocessor.from_preset(
         "bert_base_en",
@@ -37,7 +66,7 @@ def run():
     model = get_model()
     model.compile(
         loss=keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-        optimizer="adamw",
+        optimizer=BertOptimizer(),
     )
 
     return benchmark.fit(model, dataset), benchmark.predict(model, dataset)
