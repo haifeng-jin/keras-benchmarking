0ae2bac799da6826d724037b104d33aba651265e
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-12-14 22:14:09 +0000

    update

--- shell/cleanup.sh
+++ shell/cleanup.sh
@@ -8,18 +8,15 @@ venvs=(
     "keras-torch"
 )
 
-# Loop through each virtual environment
 for venv_name in "${venvs[@]}"; do
-# Check i
-if [ -d "$venv_name" ]; then
-# Deactivate the virtual environment if it's currently active
-if command -v deactivate &> /dev/null; then
-deactivate
-fi
-# Remove the virtual environment
-rm -rf "$venv_name"
-echo "Removed virtual environment: $venv_name"
-else
-echo "Virtual environment not found: $venv_name"
-fi
+    if [ -d "$venv_name" ]; then
+        if command -v deactivate &> /dev/null; then
+            deactivate
+        fi
+
+        rm -rf "$venv_name"
+        echo "Removed virtual environment: $venv_name"
+    else
+        echo "Virtual environment not found: $venv_name"
+    fi
 done
