7b42c3d9da0b9469daa7dab859479c17870e4b15
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-12-15 23:01:04 +0000

    update

--- shell/install.sh
+++ shell/install.sh
@@ -15,6 +15,9 @@ for ((i=0; i<${#venvs[@]}; i++)); do
     source ~/.venv/$venv_name/bin/activate
     pip install --upgrade pip
     pip install -r requirements/$venv_name.txt
+    if [[ $venvs_name == keras* ]]; then
+        pip install keras==3.0.1
+    fi
     deactivate
 
     echo "Installed libraries from $venv_name.txt"
