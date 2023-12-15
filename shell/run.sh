9bc28e7c25acb522387a3466643dc8e6f68be9fc
Haifeng Jin <5476582+haifeng-jin@users.noreply.github.com>
2023-12-15 23:31:38 +0000

    update

--- shell/run.sh
+++ shell/run.sh
@@ -9,16 +9,27 @@ venvs=(
     "keras-jax"
     "keras-torch"
 )
+output_file=results.txt
 
-rm results.txt
+if [ -e "$output_file" ]; then
+    rm -f "$output_file"
+fi
+
+export LD_LIBRARY_PATH=
+
 for venv_name in "${venvs[@]}"; do
-    echo "Running benchmarks for $venv_name." | tee -a results.txt
-    source $venv_path/torch/bin/activate
+    echo "Running benchmarks for $venv_name." | tee -a $output_file
+    source $venv_path/$venv_name/bin/activate
     if [[ $venv_name == torch ]]; then
         file_name=torch
     else
         file_name=keras
     fi
-    python src/bert_$file_name.py results.txt
+
+    if [[ $venv_name == keras* ]]; then
+        export KERAS_HOME=configs/${venv_name#keras-}
+    fi
+
+    python src/bert_$file_name.py $output_file
     deactivate
 done
